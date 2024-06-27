3e23e8299c6c2d8326e74e3eec71248cf78dff73
David <ds0196@uah.edu>
2024-06-27 18:57:13 -0500

    Add template project header

---
+++ include/PROJECT_TEMPLATE.h
@@ -0,0 +1,29 @@
+/**
+ * @file PROJECT_TEMPLATE.h
+ * @author your name (you@domain.com)
+ * @brief description
+ * @version 0.1
+ * @date 2024-06-27
+ * 
+ */
+#pragma once
+
+
+//---------------------//
+// PlatformIO lib_deps //
+//---------------------//
+
+
+//------//
+// LIBS //
+//------//
+
+
+//------//
+// PINS //
+//------//
+
+
+//-----------//
+// Constants //
+//-----------//
