fbc8bdda706ce92ecb8fed6d622e5cacbba8ffb4
Charles-Marmann <cmm0077@uah.edu>
2024-10-02 16:21:40 -0500

    Start creating the cpp and h file

---
+++ include/AstraCAN2.h
@@ -0,0 +1,39 @@
+//WHAT THE HELL AM I DOING.h?
+/**
+ * @file AstraCAN.h
+ * @author Charles Marmann (cmm0077@uah.edu)
+ * @brief Provides methods for interfacing with Rev SparkMax motor controllers over CAN
+ *
+ */
+#pragma once
+
+#if !__has_include("ESP32-TWAI-CAN.hpp")
+#    error Missing library! Please add the following line to lib_deps in platformio.ini:  https://github.com/tonton81/FlexCAN_T4
+
+#else
+
+#    include <ESP32-TWAI-CAN.hpp>  // https://github.com/tonton81/FlexCAN_T4
+
+
+// Core and FAERIE use CAN1, Arm uses CAN3
+#    ifdef ARM
+TwaiCAN AstraFCAN;
+#    else
+TwaiCAN AstraFCAN;
+#    endif
+
+
+// Convert float to little endian decimal representation
+void Float2LEDec(float x, uint8_t (&buffer_data)[8]);
+
+
+void identifyDevice(AstraFCAN &Can0, int can_id);
+
+
+void sendDutyCycle(AstraFCAN &Can0, int can_id, float duty_cycle);
+
+void sendHeartbeat(AstraFCAN &Can0, int can_id);
+
+void setParameter(AstraFCAN &Can0, int can_id, uint8_t paramID, uint32_t value);
+
+#endif  // __has_include
