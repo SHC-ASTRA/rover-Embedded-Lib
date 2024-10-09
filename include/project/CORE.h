580c982079c033e7095daf3acc25df6699a6d029
Charles-Marmann <cmm0077@uah.edu>
2024-10-09 11:51:59 -0500

    Define pins in core.h

--- include/project/CORE.h
+++ include/project/CORE.h
@@ -14,6 +14,15 @@
 
 #define PIN_LED_STRIP 10
 
+#define CAN_TX 12
+#define CAN_RX 13
+
+#define I2C_SCL 20
+#define I2C_SDA 22
+
+#define REV_ESP32_I 7
+#define REV_ESP32_O 8
+
 // 20 used for something with the IMU?
 
 
