3f25b3d2e3ad880e239352ad6245ab260aa72fa5
David <ds0196@uah.edu>
2024-10-13 11:22:09 -0500

    Revert main to previous state
    Charles's edits for the ESP32 are still present in esp32-dev branch

--- include/AstraCAN.h
+++ include/AstraCAN.h
@@ -1,36 +1,38 @@
-// /**
-//  * @file AstraCAN.h
-//  * @author Tristan McGinnis (tlm0047@uah.edu)
-//  * @brief Provides methods for interfacing with Rev SparkMax motor controllers over CAN
-//  *
-//  */
-// #pragma once
+/**
+ * @file AstraCAN.h
+ * @author Tristan McGinnis (tlm0047@uah.edu)
+ * @brief Provides methods for interfacing with Rev SparkMax motor controllers over CAN
+ *
+ */
+#pragma once
 
-// #if !__has_include("FlexCAN_T4.h")
-// #    error Missing library! Please add the following line to lib_deps in platformio.ini:  https://github.com/tonton81/FlexCAN_T4
+#if !__has_include("FlexCAN_T4.h")
+#    error Missing library! Please add the following line to lib_deps in platformio.ini:  https://github.com/tonton81/FlexCAN_T4
 
-// #else
+#else
 
-// #include <FlexCAN_T4.h>  // https://github.com/tonton81/FlexCAN_T4
-
-
-// // Core and FAERIE use CAN1, Arm uses CAN3
-// #    ifdef ARM
-// typedef FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> AstraFCAN;
-// #    else
-// typedef FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> AstraFCAN;
-// #    endif
+#    include <FlexCAN_T4.h>  // https://github.com/tonton81/FlexCAN_T4
 
 
-// // Convert float to little endian decimal representation
-// void Float2LEDec(float x, uint8_t (&buffer_data)[8]);
+// Core and FAERIE use CAN1, Arm uses CAN3
+#    ifdef ARM
+typedef FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> AstraFCAN;
+#    else
+typedef FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> AstraFCAN;
+#    endif
 
 
-// void identifyDevice(AstraFCAN &Can0, int can_id);
+// Convert float to little endian decimal representation
+void Float2LEDec(float x, uint8_t (&buffer_data)[8]);
 
 
-// void sendDutyCycle(AstraFCAN &Can0, int can_id, float duty_cycle);
+void identifyDevice(AstraFCAN &Can0, int can_id);
 
-// void sendHeartbeat(AstraFCAN &Can0, int can_id);
 
-// void setParameter(AstraFCAN &Can0, int can_id, uint8_t paramID, uint32_t value);
+void sendDutyCycle(AstraFCAN &Can0, int can_id, float duty_cycle);
+
+void sendHeartbeat(AstraFCAN &Can0, int can_id);
+
+void setParameter(AstraFCAN &Can0, int can_id, uint8_t paramID, uint32_t value);
+
+#endif  // __has_include
--- include/project/CORE.h
+++ include/project/CORE.h
@@ -6,22 +6,12 @@
  */
 #pragma once
 
-// Test Edit
 
 //------//
 // PINS //
 //------//
 
 #define PIN_LED_STRIP 10
-
-#define CAN_TX 12
-#define CAN_RX 13
-
-#define CAN_REV_TX 22
-#define CAN_REV_RX 23
-
-#define I2C_SCL 20
-#define I2C_SDA 22
 
 // 20 used for something with the IMU?
 
