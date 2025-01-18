4bb1b930e8cc81cf285457548447557326cd9e66
David <ds0196@uah.edu>
2025-01-18 12:15:37 -0600

    feat: add pins to CORE.h

--- include/project/CORE.h
+++ include/project/CORE.h
@@ -9,12 +9,60 @@
 
 #if defined(ESP32)
 
-#    warning "ESP32 board for Core is WIP! Did you mean to use testbed?"
+//------------------------------------------------------------------------------------------------//
+//   Feather ESP32 (URC 2025)
+//------------------------------------------------------------------------------------------------//
 
-#   define WHEEL_CIRCUMFERENCE 1.064  // Wheel's circumference in meters
+//-----------//
+// Both MCUs //
+//-----------//
+
+#    define COMMS_UART Serial1  // UART between Main-Motor
+
+#    define CAN_TX 13
+#    define CAN_RX 12
 
 
-#elif defined(CORE_TEENSY)  // URC 2024 setup
+#    if !defined(MOTORMCU)
+
+//----------//
+// Main MCU //
+//----------//
+
+// Voltage Dividers
+#        define PIN_VDIV_5V 33
+#        define PIN_VDIV_BATT 15
+#        define PIN_VDIV_12V 32
+#        define PIN_VDIV_3V3 14
+
+// I2C
+#        define I2C_SCL 20
+#        define I2C_SDA 22
+
+// LED Strip
+#        define PIN_LED_STRIP 4
+
+#    else
+
+//-----------//
+// Motor MCU //
+//-----------//
+
+#        define MOTOR_ID_FL 2  // REV motor ID for front left wheel
+#        define MOTOR_ID_FR 1  // REV motor ID for front right wheel
+#        define MOTOR_ID_BL 4  // REV motor ID for back left wheel
+#        define MOTOR_ID_BR 3  // REV motor ID for back right wheel
+
+#        define WHEEL_CIRCUMFERENCE 1.064  // Wheel's circumference in meters
+
+#    endif
+
+
+#elif defined(CORE_TEENSY)
+
+//------------------------------------------------------------------------------------------------//
+//   Teensy 4.x (URC 2024)
+//------------------------------------------------------------------------------------------------//
 
 //------//
 // Pins //
