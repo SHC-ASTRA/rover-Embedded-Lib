ccbd44a693d4e3ff08038fbf3f86a22095fc6824
Charles-Marmann <113216303+Charles-Marmann@users.noreply.github.com>
2025-01-29 19:57:47 -0600

    Added Pins and struct for setting axis speed and position

--- include/project/ARM.h
+++ include/project/ARM.h
@@ -13,21 +13,32 @@
 //   Feather ESP32 (URC 2025)
 //------------------------------------------------------------------------------------------------//
 
-#    warning "ESP32 board for Arm is WIP!"
+#   warning "ESP32 board for Arm is WIP!"
 
 //-----------//
 // Both MCUs //
 //-----------//
 
-#    define COMMS_UART Serial1  // UART between Main-Motor
-#    define MOTOR_AMOUNT 3
+#   define COMMS_UART Serial1  // UART between Main-Motor
+#   define MOTOR_AMOUNT 3
 
 
-#    if !defined(MOTORMCU)
+#   if !defined(MOTORMCU)
 
 //----------//
 // Main MCU //
 //----------//
+
+#   define ENCODER_AXIS0_PIN 22
+#   define ENCODER_AXIS1_PIN 5
+#   define ENCODER_AXIS2_PIN 6
+#   define ENCODER_AXIS3_PIN 10
+
+#   define CAN_TX 24
+#   define CAN_RX 25
+
+#   define LYNX_TX 17
+#   define LYNX_RX 18
 
 #    else
 
@@ -39,11 +50,22 @@
 #   define MOTOR_ID_A2 2
 #   define MOTOR_ID_A3 3
 
+struct IKS  // Inverse_Kinematics_Struct, instrucitons for IK
+{
+    // AxisXSpeed/Position
+    short int A1S;
+    short int A2S;
+    short int A3S;
+    short int A1P;
+    short int A2P;
+    short int A3P;
+}
+
 #   warning "Motor IDs not set"
 
 Serial1.print("Wrong Motor IDs used, they were added to resolve error, but not correctly selected");
 
-#    endif
+#   endif
 
 
 #elif defined(CORE_TEENSY)
@@ -56,21 +78,21 @@ Serial1.print("Wrong Motor IDs used, they were added to resolve error, but not c
 // Pins //
 //------//
 
-#    define PIN_AS5047P_1_CS 10
-#    define PIN_AS5047P_2_CS 37
-#    define PIN_AS5047P_3_CS 36
-#    define PIN_AXIS_0_PWM 19
+#   define PIN_AS5047P_1_CS 10
+#   define PIN_AS5047P_2_CS 37
+#   define PIN_AS5047P_3_CS 36
+#   define PIN_AXIS_0_PWM 19
 
 
 //-----------//
 // Constants //
 //-----------//
 
-#    define COMMS_UART Serial3
+#   define COMMS_UART Serial3
 
-#    define LSS_SERIAL Serial7
+#   define LSS_SERIAL Serial7
 
-#    define AS5047P_CUSTOM_SPI_BUS_SPEED 10'000'000
+#   define AS5047P_CUSTOM_SPI_BUS_SPEED 10'000'000
 
 
 #endif
